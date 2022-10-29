afaed01f13d2b87c833ca4a34bb3f3efe11399c4
Ho3ein_Khoshyari <hossein12khoshyari@gmail.com>
2022-10-29 16:32:48 +0330

    [feat] create authentication module and dashboard module and implemnet theri route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./authentication/authentication.module').then((module) => {
+        return module.AuthenticationModule;
+      }),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((module) => {
+        return module.DashboardModule;
+      }),
+  },
+  { path: '**', redirectTo: 'auth' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
