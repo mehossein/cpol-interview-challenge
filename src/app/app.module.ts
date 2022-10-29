faaa60002ac0f2e10ddde1a3f406cf52363ff82d
Ho3ein_Khoshyari <hossein12khoshyari@gmail.com>
2022-10-29 14:54:41 +0330

    [feat] add ngx-toaster packge and import in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// App
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
+
+// Third-Party
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    // Angular
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // Third-Party
+    ToastrModule.forRoot(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
