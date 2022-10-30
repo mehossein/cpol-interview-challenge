51d02612eefb349c507a38041e0029d62c68d71a
Ho3ein_Khoshyari <hossein12khoshyari@gmail.com>
2022-10-30 08:59:09 +0330

    [feat] implement authentication module

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,35 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// App
+import { AuthApi } from '@app/authentication/Api';
+import { LoginComponent } from './components/login/login.component';
+import { AuthenticationRoutingModule } from './authentication.routing';
+import { ContainerComponent } from './components/container/container.component';
 
+// Third-Party
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, ContainerComponent],
   imports: [
-    CommonModule
-  ]
+    // Angular
+    FormsModule,
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+
+    // App
+    AuthenticationRoutingModule,
+
+    // Third-Party
+    MatInputModule,
+    MatFormFieldModule,
+  ],
+  providers: [AuthApi],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
