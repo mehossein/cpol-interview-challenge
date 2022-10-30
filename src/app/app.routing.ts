ace5e3a240a180064765821cbd35fc526662b9ed
Ho3ein_Khoshyari <hossein12khoshyari@gmail.com>
2022-10-30 08:59:40 +0330

    [feat] add submodules routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,29 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// App
+import { AuthGuard } from './authentication/shared/guard';
+
 const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren: () =>
-      import('./authentication/authentication.module').then((module) => {
-        return module.AuthenticationModule;
-      }),
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth',
   },
   {
     path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((module) => {
         return module.DashboardModule;
+      }),
+  },
+  {
+    path: 'auth',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./authentication/authentication.module').then((module) => {
+        return module.AuthenticationModule;
       }),
   },
   { path: '**', redirectTo: 'auth' },
